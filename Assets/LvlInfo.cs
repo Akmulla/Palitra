1c1dd5df66e73d4777af9168542deaeb1d8261a9
Akmulla <d4n73@yandex.ru>
2017-08-25 06:35:54 +0600

    Отображение текущего уровня

---
+++ Assets/LvlInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LvlInfo : MonoBehaviour
+{
+    public Text text;
+    int lines_passed;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.text = GameController.game_controller.GetCurrentLvl().ToString();
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("ChangeLvl", LvlChanged);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("ChangeLvl", LvlChanged);
+    }
+
+    void LvlChanged()
+    {
+        text.text = GameController.game_controller.GetCurrentLvl().ToString();
+    }
+}
