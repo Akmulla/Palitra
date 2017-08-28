7083511d3cb79f389e070ca108198980935c08ac
Akmulla <d4n73@yandex.ru>
2017-08-28 18:00:41 +0600

    anim

---
+++ Assets/Scripts/ChngLvlAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChngLvlAnim : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("ChangeLvl",ChangeLvl);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("ChangeLvl", ChangeLvl);
+    }
+
+    void ChangeLvl()
+    {
+        anim.SetTrigger("ChangeLvl");
+    }
+}
