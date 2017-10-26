666f8e5df7f5079b42ec4c5422d0fd438bb54cee
Akmulla <d4n73@yandex.ru>
2017-10-26 16:58:11 +0600

    roll update

--- Assets/Scripts/Lines/RollAway.cs
+++ Assets/Scripts/Lines/RollAway.cs
@@ -7,7 +7,9 @@ public class RollAway : MonoBehaviour
     public bool right;
     bool rolling;
     float move_speed=3.0f;
-    float rotate_speed=3.0f;
+    float rotate_speed=60.0f;
+    Vector3 dir_right=new Vector3(1.0f,1.0f,0.0f);
+    Vector3 dir_left = new Vector3(-1.0f, 1.0f, 0.0f);
 
     public void Roll()
     {
@@ -19,8 +21,16 @@ public class RollAway : MonoBehaviour
         if (!rolling)
             return;
 
-        transform.position += Vector3.right * move_speed * Time.deltaTime;
-        transform.Rotate(new Vector3(0.0f,0.0f,60.0f*Time.deltaTime));
+        //transform.position += Vector3.right * move_speed * Time.deltaTime;
+        if (right)
+        {
+            transform.position += dir_right.normalized * move_speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += dir_left.normalized * move_speed * Time.deltaTime;
+        }
+        transform.Rotate(new Vector3(0.0f,0.0f,rotate_speed*Time.deltaTime));
     }
 
     void OnEnable()
