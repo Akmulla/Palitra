891451ab9725473e9d85e8ffe94dd100cfe05b21
Akmulla <d4n73@yandex.ru>
2017-11-19 17:19:01 +0600

    update

--- Assets/Rate.cs
+++ Assets/Rate.cs
@@ -9,7 +9,8 @@ public class Rate : MonoBehaviour
     {
         // Application.OpenURL("market://details?id=" + Application.productName);
 #if UNITY_ANDROID
-     Application.OpenURL("market://details?id=" + Application.productName);
+     //Application.OpenURL("market://details?id=" + Application.productName);
+        Application.OpenURL("https://play.google.com/store/apps/details?id=secret.com.secretbox.palette"); 
 #elif UNITY_IPHONE
      Application.OpenURL("itms-apps://itunes.apple.com/app/id1306735206");
 #endif
