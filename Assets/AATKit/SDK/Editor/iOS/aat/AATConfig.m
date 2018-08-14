8f734681e6ac01e9919538ac06dcd773577b4069
Akmulla <d4n73@yandex.ru>
2018-08-14 15:05:26 -0600

    asdfsdg

---
+++ Assets/AATKit/SDK/Editor/iOS/aat/AATConfig.m
@@ -0,0 +1,46 @@
+//
+//  AATConfig.m
+//  IntentKit
+//
+//  Created by Daniel Brockhaus on 30.01.13.
+//  Copyright (c) 2013 Daniel Brockhaus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+BOOL aat_use_Admob          = YES;
+BOOL aat_use_AdColony       = YES;
+BOOL aat_use_AdX            = YES;
+BOOL aat_use_Amazon         = YES;
+BOOL aat_use_Applovin       = YES;
+BOOL aat_use_Appnexus       = YES;
+BOOL aat_use_Apprupt        = YES;
+BOOL aat_use_DFP            = YES;
+BOOL aat_use_Facebook       = YES;
+BOOL aat_use_Flurry         = YES;
+BOOL aat_use_Inmobi         = YES;
+BOOL aat_use_Inneractive    = YES;
+BOOL aat_use_LoopMe         = YES;
+BOOL aat_use_MdotM          = YES;
+BOOL aat_use_Millennial     = YES;
+BOOL aat_use_MoPub          = YES;
+BOOL aat_use_Nexage         = YES;
+BOOL aat_use_OpenX          = YES;
+BOOL aat_use_Permodo        = YES;
+BOOL aat_use_PubMatic       = YES;
+BOOL aat_use_Revmob         = YES;
+BOOL aat_use_Smaato         = YES;
+BOOL aat_use_SmartAdServer  = YES;
+BOOL aat_use_SmartStream    = YES;
+BOOL aat_use_Unity          = YES;
+
+
+
+#if 0   // please enable this when NOT using Millennial
+NSString *MillennialMediaAdWasTapped;
+NSString *MillennialMediaAdObjectKey;
+NSString *MillennialMediaAdModalWillAppear;
+NSString *MillennialMediaAdModalDidDismiss;
+#endif
