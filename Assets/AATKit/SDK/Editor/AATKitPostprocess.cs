b905e282590aab6b1fd0d89fc21a1ede495736dd
Akmulla <d4n73@yandex.ru>
2018-08-19 23:58:55 -0600

    fdg

--- Assets/AATKit/SDK/Editor/AATKitPostprocess.cs
+++ Assets/AATKit/SDK/Editor/AATKitPostprocess.cs
@@ -22,7 +22,7 @@ public class AATKitPostprocess : MonoBehaviour
 
 	private static string projPath;
 
-	[PostProcessBuildAttribute(999)]
+	[PostProcessBuildAttribute(9999)]
 	public static void OnPostprocessBuild(BuildTarget target, string pathToBuildProject)
 	{        
 		if (target == BuildTarget.iOS) 
